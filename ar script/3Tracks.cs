c23ad3d99411119d196078c823cd56d98e228284
kianzzarrin <kian.zarrin@gmail.com>
2021-12-07 18:27:54 +0200

    Add project files.

---
+++ ar script/3Tracks.cs
@@ -0,0 +1,20 @@
+﻿using AdaptiveRoads.Manager;
+namespace Zeldslayer {
+    using AdaptiveRoads.CustomScript;
+    using System.Linq;
+    using static NetLaneExt.Flags;
+    using static NetLane.Flags;
+    using static NetSegmentExt.Flags;
+    using static NetSegment.Flags;
+    using static NetSegmentEnd.Flags;
+    using static NetNodeExt.Flags;
+    using static NetNode.Flags;
+    using KianCommons;
+    using ColossalFramework;
+
+    public class Tracks3 : PredicateBase {
+        public override bool Condition() => TrackCount == 3;
+        int TrackCount => Node.VanillaNode.Info.m_lanes.Count(IsTrack);
+        bool IsTrack(NetInfo.Lane laneInfo) => laneInfo.m_vehicleType.IsFlagSet(VehicleInfo.VehicleType.Train);
+    }
+}
---
+++ ar script/TailNoLHT.cs
@@ -0,0 +1,15 @@
+﻿using AdaptiveRoads.Manager;
+namespace Nien  {
+    using AdaptiveRoads.CustomScript;
+    using static NetLaneExt.Flags;
+    using static NetLane.Flags;
+    using static NetSegmentExt.Flags;
+    using static NetSegment.Flags;
+    using static NetSegmentEnd.Flags;
+    using static NetNodeExt.Flags;
+    using static NetNode.Flags;
+
+    public class TailNoLHT : PredicateBase {
+        public override bool Condition() => SegmentEnd.Has(IsTailNode) ^ SegmentEnd.Has(LeftHandTraffic);
+    }
+}
