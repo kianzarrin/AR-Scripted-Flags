3350b4cf78b0068dc97b15b70763d16903b44202
kianzzarrin <kian.zarrin@gmail.com>
2021-12-29 12:02:22 +0200

    c0l1

---
+++ ar script/Custom0L1.cs
@@ -0,0 +1,16 @@
+﻿using AdaptiveRoads.Manager;
+namespace Hockenheim {
+    using AdaptiveRoads.CustomScript;
+    using static NetLaneExt.Flags;
+    using static NetLane.Flags;
+        using static NetSegmentExt.Flags;
+    using static NetSegment.Flags;
+    using static NetSegmentEnd.Flags;
+    using static NetNodeExt.Flags;
+    using static NetNode.Flags;
+    using System;
+
+    public class Custom0L1 : PredicateBase {
+        public override bool Condition() => Lanes(1).Has(NetLaneExt.Flags.Custom0);
+    }
+}
