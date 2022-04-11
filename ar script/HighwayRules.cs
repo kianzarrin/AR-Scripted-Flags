5180a3aa4a4d8637f5776c8df346f9a1ba3aadf6
kianzzarrin <kian.zarrin@gmail.com>
2022-04-11 23:18:38 +0300

    highway rules

---
+++ ar script/HighwayRules.cs
@@ -0,0 +1,6 @@
+﻿namespace Hock {
+    using AdaptiveRoads.CustomScript;
+    public class HighwayRules : PredicateBase {
+        public override bool Condition() => TrafficManager.State.Options.highwayRules;
+    }
+}
