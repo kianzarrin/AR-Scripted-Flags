10222266d795745b4744c12c8e553ddabc20cca8
kianzzarrin <kian.zarrin@gmail.com>
2022-04-11 23:04:33 +0300

    OnRampTracks

--- ar script/OnRampTrack.cs
+++ ar script/OnRampTrack.cs
@@ -9,10 +9,28 @@ namespace Hock {
     using static NetNodeExt.Flags;
     using static NetNode.Flags;
     using System;
+    using AdaptiveRoads.Util;
+    using TrafficManager.API.Manager;
+    using KianCommons;
 
     public class OnRampTrack : PredicateBase {
+        static IManagerFactory TMPE => TrafficManager.API.Implementations.ManagerFactory;
+        bool HighwayRules => TrafficManager.State.Options.highwayRules;
+
+        static int OuterSimilarLaneIndex(NetInfo.Lane laneInfo) =>
+            laneInfo.m_similarLaneCount - laneInfo.m_similarLaneIndex - 1;
+        
+        static int OuterSimilarLaneIndex(uint laneId) {
+            return OuterSimilarLaneIndex(NetUtil.GetLaneInfo(laneId));
+        }
+
         public override bool Condition() {
-            return true;
+            if (HighwayRules) {
+                var routings = TMPEHelpers.GetForwardRoutings(LaneId, Segment.Head.NodeID);
+                return routings.Length == 1 && OuterSimilarLaneIndex(routings[0].laneId) == 0; // outermost lane
+            } else {
+                return false;
+            }
         }
     }
 }
